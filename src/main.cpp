c2da3c2ddd4e0f04a3a2fef8d51af495de1e1cb3
hayk <haykh.astro@gmail.com>
2024-11-05 17:11:17 -0500

    buff sort changed

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "global.hpp"
 #include "particle_tracker.hpp"
-#include "sorter_entity.hpp"
 #include "sorter_buffer.hpp"
+#include "sorter_entity.hpp"
 #include "sorter_thrust.hpp"
 #include "timer.hpp"
 
 #include <Kokkos_Core.hpp>
+
 #include <iostream>
 
 auto main(int argc, char* argv[]) -> int {
@@ -14,56 +15,54 @@ auto main(int argc, char* argv[]) -> int {
   int ny = 70;
   int nz = 63;
 
-    Kokkos::initialize(argc, argv);
-    {
-      const std::size_t nparticles = static_cast<int>(argc) > 1 ? std::stoi(argv[1]) : 100;
-      real_t            dt         = 5.0;
+  Kokkos::initialize(argc, argv);
+  {
+    const std::size_t nparticles = argc > 1 ? std::stoi(argv[1]) : 100;
 
-      std::cout << "Number of particles: " << nparticles << std::endl;
-      // Initialize Particle Arrays
-      Kokkos::View<short*>          tag_arr("Tag array", nparticles);
-      Kokkos::View<int*>            i_arr("i-position array", nparticles);
-      Kokkos::View<int*>            j_arr("j-position array", nparticles);
-      Kokkos::View<int*>            k_arr("k-position array", nparticles);
-      Kokkos::View<float*>          dx_arr("dx-position array", nparticles);
-      Kokkos::View<float*>          dy_arr("dy-position array", nparticles);
-      Kokkos::View<float*>          dz_arr("dz-position array", nparticles);
-      Kokkos::View<real_t*>         vx_arr("x-velocity array", nparticles);
-      Kokkos::View<real_t*>         vy_arr("y-velocity array", nparticles);
-      Kokkos::View<real_t*>         vz_arr("z-velocity array", nparticles);
-      Kokkos::View<std::size_t[29]> tag_ctr_cumsum("Tag counter array");
+    real_t dt = 5.0;
 
-      InitializeParticleArrays(nparticles,
-                              { 0, nx, 0, ny, 0, nz },
-                              tag_arr,
-                              i_arr,
-                              j_arr,
-                              k_arr,
-                              dx_arr,
-                              dy_arr,
-                              dz_arr,
-                              vx_arr,
-                              vy_arr,
-                              vz_arr);
+    std::cout << "Number of particles: " << nparticles << std::endl;
+    // Initialize Particle Arrays
+    Kokkos::View<short*>          tag_arr("Tag array", nparticles);
+    Kokkos::View<int*>            i_arr("i-position array", nparticles);
+    Kokkos::View<int*>            j_arr("j-position array", nparticles);
+    Kokkos::View<int*>            k_arr("k-position array", nparticles);
+    Kokkos::View<float*>          dx_arr("dx-position array", nparticles);
+    Kokkos::View<float*>          dy_arr("dy-position array", nparticles);
+    Kokkos::View<float*>          dz_arr("dz-position array", nparticles);
+    Kokkos::View<real_t*>         vx_arr("x-velocity array", nparticles);
+    Kokkos::View<real_t*>         vy_arr("y-velocity array", nparticles);
+    Kokkos::View<real_t*>         vz_arr("z-velocity array", nparticles);
+    Kokkos::View<std::size_t[29]> tag_ctr_cumsum("Tag counter array");
 
-      PushParticles(nparticles,
-                    { 0, nx, 0, ny, 0, nz },
-                    tag_ctr_cumsum,
-                    tag_arr,
-                    i_arr,
-                    j_arr,
-                    k_arr,
-                    dx_arr,
-                    dy_arr,
-                    dz_arr,
-                    vx_arr,
-                    vy_arr,
-                    vz_arr,
-                    dt);
-      Kokkos::fence();
-      for (int iteration=0; iteration<20; iteration++)
-      {
-        SortThrust(nparticles,
+    InitializeParticleArrays(nparticles,
+                             { 0, nx, 0, ny, 0, nz },
+                             tag_arr,
+                             i_arr,
+                             j_arr,
+                             k_arr,
+                             dx_arr,
+                             dy_arr,
+                             dz_arr,
+                             vx_arr,
+                             vy_arr,
+                             vz_arr);
+
+    Kokkos::View<short*>          tag_arr_bckp("Tag array", nparticles);
+    Kokkos::View<int*>            i_arr_bckp("i-position array", nparticles);
+    Kokkos::View<int*>            j_arr_bckp("j-position array", nparticles);
+    Kokkos::View<int*>            k_arr_bckp("k-position array", nparticles);
+    Kokkos::View<float*>          dx_arr_bckp("dx-position array", nparticles);
+    Kokkos::View<float*>          dy_arr_bckp("dy-position array", nparticles);
+    Kokkos::View<float*>          dz_arr_bckp("dz-position array", nparticles);
+    Kokkos::View<real_t*>         vx_arr_bckp("x-velocity array", nparticles);
+    Kokkos::View<real_t*>         vy_arr_bckp("y-velocity array", nparticles);
+    Kokkos::View<real_t*>         vz_arr_bckp("z-velocity array", nparticles);
+    Kokkos::View<std::size_t[29]> tag_ctr_cumsum_bckp("Tag counter array");
+
+    PushParticles(nparticles,
+                  { 0, nx, 0, ny, 0, nz },
+                  tag_ctr_cumsum,
                   tag_arr,
                   i_arr,
                   j_arr,
@@ -73,43 +72,96 @@ auto main(int argc, char* argv[]) -> int {
                   dz_arr,
                   vx_arr,
                   vy_arr,
-                  vz_arr);
-        Kokkos::fence();
-      }
-      for (int iteration=0; iteration<20; iteration++)
-      {
-        SortBuffer( nparticles,
-                    tag_arr, 
-                    tag_ctr_cumsum, 
-                    i_arr, 
-                    j_arr, 
-                    k_arr, 
-                    dx_arr, 
-                    dy_arr, 
-                    dz_arr, 
-                    vx_arr, 
-                    vy_arr, 
-                    vz_arr);
-        Kokkos::fence();
-      }
-      for (int iteration=0; iteration<20; iteration++)
-      {
-        SortEntity( nparticles,
-                    tag_arr,
-                    i_arr,
-                    j_arr,
-                    k_arr,
-                    dx_arr,
-                    dy_arr,
-                    dz_arr,
-                    vx_arr,
-                    vy_arr,
-                    vz_arr);
-        Kokkos::fence();
-      }
-      }
-    Kokkos::finalize();
-  
+                  vz_arr,
+                  dt);
+
+    Kokkos::deep_copy(tag_arr_bckp, tag_arr);
+    Kokkos::deep_copy(i_arr_bckp, i_arr);
+    Kokkos::deep_copy(j_arr_bckp, j_arr);
+    Kokkos::deep_copy(k_arr_bckp, k_arr);
+    Kokkos::deep_copy(dx_arr_bckp, dx_arr);
+    Kokkos::deep_copy(dy_arr_bckp, dy_arr);
+    Kokkos::deep_copy(dz_arr_bckp, dz_arr);
+    Kokkos::deep_copy(vx_arr_bckp, vx_arr);
+    Kokkos::deep_copy(vy_arr_bckp, vy_arr);
+    Kokkos::deep_copy(vz_arr_bckp, vz_arr);
+    Kokkos::deep_copy(tag_ctr_cumsum_bckp, tag_ctr_cumsum);
+
+    // for (int iteration = 0; iteration < 20; iteration++) {
+    //   SortThrust(nparticles,
+    //              tag_arr,
+    //              i_arr,
+    //              j_arr,
+    //              k_arr,
+    //              dx_arr,
+    //              dy_arr,
+    //              dz_arr,
+    //              vx_arr,
+    //              vy_arr,
+    //              vz_arr);
+    //   Kokkos::deep_copy(tag_arr, tag_arr_bckp);
+    //   Kokkos::deep_copy(i_arr, i_arr_bckp);
+    //   Kokkos::deep_copy(j_arr, j_arr_bckp);
+    //   Kokkos::deep_copy(k_arr, k_arr_bckp);
+    //   Kokkos::deep_copy(dx_arr, dx_arr_bckp);
+    //   Kokkos::deep_copy(dy_arr, dy_arr_bckp);
+    //   Kokkos::deep_copy(dz_arr, dz_arr_bckp);
+    //   Kokkos::deep_copy(vx_arr, vx_arr_bckp);
+    //   Kokkos::deep_copy(vy_arr, vy_arr_bckp);
+    //   Kokkos::deep_copy(vz_arr, vz_arr_bckp);
+    //   Kokkos::deep_copy(tag_ctr_cumsum, tag_ctr_cumsum_bckp);
+    // }
+    for (int iteration = 0; iteration < 20; iteration++) {
+      SortBuffer(nparticles,
+                 tag_arr,
+                 tag_ctr_cumsum,
+                 i_arr,
+                 j_arr,
+                 k_arr,
+                 dx_arr,
+                 dy_arr,
+                 dz_arr,
+                 vx_arr,
+                 vy_arr,
+                 vz_arr);
+      Kokkos::deep_copy(tag_arr, tag_arr_bckp);
+      Kokkos::deep_copy(i_arr, i_arr_bckp);
+      Kokkos::deep_copy(j_arr, j_arr_bckp);
+      Kokkos::deep_copy(k_arr, k_arr_bckp);
+      Kokkos::deep_copy(dx_arr, dx_arr_bckp);
+      Kokkos::deep_copy(dy_arr, dy_arr_bckp);
+      Kokkos::deep_copy(dz_arr, dz_arr_bckp);
+      Kokkos::deep_copy(vx_arr, vx_arr_bckp);
+      Kokkos::deep_copy(vy_arr, vy_arr_bckp);
+      Kokkos::deep_copy(vz_arr, vz_arr_bckp);
+      Kokkos::deep_copy(tag_ctr_cumsum, tag_ctr_cumsum_bckp);
+    }
+    for (int iteration = 0; iteration < 20; iteration++) {
+      SortEntity(nparticles,
+                 tag_arr,
+                 i_arr,
+                 j_arr,
+                 k_arr,
+                 dx_arr,
+                 dy_arr,
+                 dz_arr,
+                 vx_arr,
+                 vy_arr,
+                 vz_arr);
+      Kokkos::deep_copy(tag_arr, tag_arr_bckp);
+      Kokkos::deep_copy(i_arr, i_arr_bckp);
+      Kokkos::deep_copy(j_arr, j_arr_bckp);
+      Kokkos::deep_copy(k_arr, k_arr_bckp);
+      Kokkos::deep_copy(dx_arr, dx_arr_bckp);
+      Kokkos::deep_copy(dy_arr, dy_arr_bckp);
+      Kokkos::deep_copy(dz_arr, dz_arr_bckp);
+      Kokkos::deep_copy(vx_arr, vx_arr_bckp);
+      Kokkos::deep_copy(vy_arr, vy_arr_bckp);
+      Kokkos::deep_copy(vz_arr, vz_arr_bckp);
+      Kokkos::deep_copy(tag_ctr_cumsum, tag_ctr_cumsum_bckp);
+    }
+  }
+  Kokkos::finalize();
 
   return 0;
 }
--- src/particle_tracker/sorter_buffer.cpp
+++ src/particle_tracker/sorter_buffer.cpp
@@ -10,56 +10,65 @@
         count per tag and then populate the buffers
         by traversing through the array
 */
-void SortBuffer(std::size_t                     nparticles,
-                Kokkos::View<short*>            tag_arr,
-                const Kokkos::View<size_t[29]>  tag_ctr_cumsum,
-                Kokkos::View<int*>              i_arr,
-                Kokkos::View<int*>              j_arr,
-                Kokkos::View<int*>              k_arr,
-                Kokkos::View<float*>            dx_arr,
-                Kokkos::View<float*>            dy_arr,
-                Kokkos::View<float*>            dz_arr,
-                Kokkos::View<real_t*>           vx_arr,
-                Kokkos::View<real_t*>           vy_arr,
-                Kokkos::View<real_t*>           vz_arr)
-{         
+void SortBuffer(std::size_t                    nparticles,
+                Kokkos::View<short*>           tag_arr,
+                const Kokkos::View<size_t[29]> tag_ctr_cumsum,
+                Kokkos::View<int*>             i_arr,
+                Kokkos::View<int*>             j_arr,
+                Kokkos::View<int*>             k_arr,
+                Kokkos::View<float*>           dx_arr,
+                Kokkos::View<float*>           dy_arr,
+                Kokkos::View<float*>           dz_arr,
+                Kokkos::View<real_t*>          vx_arr,
+                Kokkos::View<real_t*>          vy_arr,
+                Kokkos::View<real_t*>          vz_arr) {
+  auto tag_ctr_cumsum_h = Kokkos::create_mirror_view(tag_ctr_cumsum);
+  Kokkos::deep_copy(tag_ctr_cumsum_h, tag_ctr_cumsum);
+  const auto buffsize = tag_ctr_cumsum_h(28) - tag_ctr_cumsum_h(2);
+  // const auto buffsize = nparticles;
+  std::cout << "Buffer size: " << buffsize << std::endl;
+
   TIMER_START(SortBuffer);
 
   // Allocate buffers
-  Kokkos::View<short*>      tag_buffer("tag_buffer", nparticles);
-  Kokkos::View<int*>        i_buffer("i_buffer", nparticles);
-  Kokkos::View<int*>        j_buffer("j_buffer", nparticles);
-  Kokkos::View<int*>        k_buffer("k_buffer", nparticles);
-  Kokkos::View<float*>      dx_buffer("dx_buffer", nparticles);
-  Kokkos::View<float*>      dy_buffer("dy_buffer", nparticles);
-  Kokkos::View<float*>      dz_buffer("dz_buffer", nparticles);
-  Kokkos::View<real_t*>     vx_buffer("vx_buffer", nparticles);
-  Kokkos::View<real_t*>     vy_buffer("vy_buffer", nparticles);
-  Kokkos::View<real_t*>     vz_buffer("vz_buffer", nparticles);
+  Kokkos::View<short*>  tag_buffer("tag_buffer", buffsize);
+  Kokkos::View<int*>    i_buffer("i_buffer", buffsize);
+  Kokkos::View<int*>    j_buffer("j_buffer", buffsize);
+  Kokkos::View<int*>    k_buffer("k_buffer", buffsize);
+  Kokkos::View<float*>  dx_buffer("dx_buffer", buffsize);
+  Kokkos::View<float*>  dy_buffer("dy_buffer", buffsize);
+  Kokkos::View<float*>  dz_buffer("dz_buffer", buffsize);
+  Kokkos::View<real_t*> vx_buffer("vx_buffer", buffsize);
+  Kokkos::View<real_t*> vy_buffer("vy_buffer", buffsize);
+  Kokkos::View<real_t*> vz_buffer("vz_buffer", buffsize);
 
   // Array to keep track of number of elements initialized in the buffer
-  Kokkos::View<size_t[28]>       buffer_ctr("buffer_ctr");
+  Kokkos::View<size_t[28]> buffer_ctr("buffer_ctr");
 
   // Begin populating
   Kokkos::parallel_for(
     "Particle pusher loop",
     Kokkos::RangePolicy<Kokkos::Cuda>(0, nparticles),
     KOKKOS_LAMBDA(const std::size_t p) {
+      if (tag_arr(p) < 2) {
+        return;
+      }
+      const auto tag          = tag_arr(p);
+      const auto local_index  = Kokkos::atomic_fetch_add(&buffer_ctr(tag), 1);
+      // const auto global_index = tag_ctr_cumsum(tag) + local_index;
+      const auto global_index = tag_ctr_cumsum(tag) - tag_ctr_cumsum(2) +
+                                local_index;
 
-    const auto tag            = tag_arr(p);
-    const auto local_index    = Kokkos::atomic_fetch_add(&buffer_ctr(tag), 1);
-    const auto global_index   = tag_ctr_cumsum(tag) + local_index;
-
-    tag_buffer(global_index)  = tag;
-    i_buffer(global_index)    = i_arr(p);
-    j_buffer(global_index)    = j_arr(p);
-    k_buffer(global_index)    = k_arr(p);
-    dx_buffer(global_index)   = dx_arr(p);
-    dy_buffer(global_index)   = dy_arr(p);
-    dz_buffer(global_index)   = dz_arr(p);
-    vx_buffer(global_index)   = vx_arr(p);
-    vy_buffer(global_index)   = vy_arr(p);
-    vz_buffer(global_index)   = vz_arr(p);
+      tag_buffer(global_index) = tag;
+      i_buffer(global_index)   = i_arr(p);
+      j_buffer(global_index)   = j_arr(p);
+      k_buffer(global_index)   = k_arr(p);
+      dx_buffer(global_index)  = dx_arr(p);
+      dy_buffer(global_index)  = dy_arr(p);
+      dz_buffer(global_index)  = dz_arr(p);
+      vx_buffer(global_index)  = vx_arr(p);
+      vy_buffer(global_index)  = vy_arr(p);
+      vz_buffer(global_index)  = vz_arr(p);
     });
 
   // Copy elements from buffer arrays to main arrays
@@ -80,19 +89,19 @@ void SortBuffer(std::size_t                     nparticles,
       vz_arr(p)  = vz_buffer(p);
     });
   */
-  
+
   // Copy back to original arrays
-  tag_arr                     = tag_buffer;
-  i_arr                       = i_buffer;
-  j_arr                       = j_buffer;
-  k_arr                       = k_buffer;
-  dx_arr                      = dx_buffer;
-  dy_arr                      = dy_buffer;
-  dz_arr                      = dz_buffer;
-  vx_arr                      = vx_buffer;
-  vy_arr                      = vy_buffer;
-  vz_arr                      = vz_buffer;
-  
+  // Kokkos::deep_copy(tag_arr, tag_buffer);
+  // Kokkos::deep_copy(i_arr, i_buffer);
+  // Kokkos::deep_copy(j_arr, j_buffer);
+  // Kokkos::deep_copy(k_arr, k_buffer);
+  // Kokkos::deep_copy(dx_arr, dx_buffer);
+  // Kokkos::deep_copy(dy_arr, dy_buffer);
+  // Kokkos::deep_copy(dz_arr, dz_buffer);
+  // Kokkos::deep_copy(vx_arr, vx_buffer);
+  // Kokkos::deep_copy(vy_arr, vy_buffer);
+  // Kokkos::deep_copy(vz_arr, vz_buffer);
+
   TIMER_STOP(SortBuffer);
   std::cout << "Particles sorted\n";
   return;
